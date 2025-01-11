bc546f44217069ff8b89664c055e197d8b468708
Jordan Kniest <j.kniest@basecom.de>
2025-01-11 12:54:04 +0100

    Add i18n support

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,8 +1,15 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ['../src/module', '@nuxtjs/i18n'],
+
   ducktory: {
     enabled: true,
     debug: true,
+    path: '/ducktory',
   },
   devtools: { enabled: true },
+  i18n: {
+    locales: ['en', 'de'],
+    strategy: 'prefix',
+    vueI18n: './i18n.config.ts',
+  },
 })
--- playground/pages/index.vue
+++ playground/pages/index.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <NuxtLayout>
     <h1>Index Page</h1>
-    <ducktory-story-1 />
+    <div>Hello World.</div>
   </NuxtLayout>
 </template>
