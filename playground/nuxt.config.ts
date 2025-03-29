c6753284cb145c1c46759c5eb765aa46365f432b
Jordan Kniest <mail@jkniest.de>
2025-03-29 18:25:06 +0100

    Update all dependencies, migrate to bun text lockfile, change tailwind for max compatibility

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -5,10 +5,14 @@ export default defineNuxtConfig({
     enabled: true,
     debug: true,
   },
+
   devtools: { enabled: true },
+
   i18n: {
     locales: ['en', 'de'],
     strategy: 'prefix',
     vueI18n: './i18n.config.ts',
   },
+
+  compatibilityDate: '2025-03-29',
 })
