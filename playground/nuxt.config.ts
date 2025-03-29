10b67c4253117712d04bff5c335ce889c711ad13
Jordan Kniest <mail@jkniest.de>
2025-03-29 18:29:25 +0100

    Update i18n

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,18 +1,30 @@
 export default defineNuxtConfig({
   modules: ['../src/module', '@nuxtjs/i18n'],
 
+  devtools: { enabled: true },
+
+  compatibilityDate: '2025-03-29',
+
   ducktory: {
     enabled: true,
     debug: true,
   },
 
-  devtools: { enabled: true },
-
   i18n: {
-    locales: ['en', 'de'],
-    strategy: 'prefix',
     vueI18n: './i18n.config.ts',
+    strategy: 'prefix_except_default',
+    defaultLocale: 'de-DE',
+    detectBrowserLanguage: false,
+    locales: [
+      {
+        code: 'de-DE',
+        files: ['de-DE/messages.json'],
+      },
+      {
+        code: 'en-GB',
+        files: ['en-GB/messages.json'],
+      },
+    ],
+    lazy: true,
   },
-
-  compatibilityDate: '2025-03-29',
 })
